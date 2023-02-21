0c6e032547697e3e6d886fc000d9203ae212d911
Carlos Muniz <profcpmuniz@gmail.com>
2023-02-21 20:34:20 +0000

    Federico Question about FontAnswone

--- my-review-app/src/App.js
+++ my-review-app/src/App.js
@@ -1,10 +1,12 @@
 import MyFirstComponent from "./components/MyFirstComponent";
 import MySecondComponent from "./components/MySecondComponent";
-
+import { FaBeer, FaDigitalOcean } from 'react-icons/fa';
+import { MdCommute } from "react-icons/md";
 function App() {
   return (
     <div>
-      <div>Hello World</div>
+      <div>  <i className="fa-solid fa-clock"></i> Hello World <FaBeer  size={70}  /> <FaDigitalOcean /></div>
+      <MdCommute/>
       <MyFirstComponent message="Carlos"/>
       <MyFirstComponent message="Muniz"/>
       <MyFirstComponent/>
